9c9a947969cf6c6299e5c5b258d9bc2ac0528a26
zoumingzhe <zoumingzhe@qq.com>
2018-10-02 15:56:16 +0800

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except ImportError:
 setup(
     name="ztools",
     description="ToolBox for Python, Easy to Use.",
-    version="1.0",
+    version="1.0.1",
     author="Zou Mingzhe",
     author_email="zoumingzhe@qq.com",
     url="https://zoumingzhe.github.io/",
