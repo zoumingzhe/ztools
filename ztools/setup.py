9add9b0437c44d6acb3255e39737038d3444d011
zoumingzhe <zoumingzhe@qq.com>
2018-10-04 21:21:51 +0800

    setup version=1.1.3

--- ztools/setup.py
+++ ztools/setup.py
@@ -18,7 +18,7 @@ except ImportError:
 setup(
     name="ztools",
     description="ToolBox for Python, Easy to Use.",
-    version="1.1.2",
+    version="1.1.3",
     author="Zou Mingzhe",
     author_email="zoumingzhe@qq.com",
     url="https://github.com/zoumingzhe/ztools",
