107568853b18e86ae0162142deda8ba3a1f2e105
zoumingzhe <zoumingzhe@qq.com>
2018-10-04 20:12:40 +0800

    delete

--- examples/Version.py
+++
@@ -1,9 +0,0 @@
-import ztools
-
-ztools_timeout = ztools.Timeout()
-ztools_timeout.Version(isShow = True)
-
-ztools_progressbar = ztools.ProgressBar()
-ztools_progressbar.Version(isShow = True)
-
-input()
