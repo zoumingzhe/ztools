41c52595218b1a4565486697cfb80032057d3af9
zoumingzhe <zoumingzhe@qq.com>
2018-10-04 19:42:24 +0800

    add
    example

---
+++ examples/Version.py
@@ -0,0 +1,9 @@
+import ztools
+
+ztools_timeout = ztools.Timeout()
+ztools_timeout.Version(isShow = True)
+
+ztools_progressbar = ztools.ProgressBar()
+ztools_progressbar.Version(isShow = True)
+
+input()
