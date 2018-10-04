6760cf4efab87d6bcbaa16eec040de1f1a841dfb
zoumingzhe <zoumingzhe@qq.com>
2018-10-04 20:13:01 +0800

    add
    version

---
+++ examples/version.py
@@ -0,0 +1,11 @@
+import ztools
+
+# 显示 Timeout 版本号
+ztools_timeout = ztools.Timeout()
+ztools_timeout.Version(isShow = True)
+
+# 显示 ProgressBar 版本号
+ztools_progressbar = ztools.ProgressBar()
+ztools_progressbar.Version(isShow = True)
+
+input()
