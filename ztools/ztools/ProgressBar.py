35d2f8c3d59014b184bdc2d9e14f508b1084c982
Mingzhe Zou <zoumingzhe@qq.com>
2019-02-11 06:13:36 +0800

    更新源代码
    增加help代码，无其它更改

--- ztools/ztools/ProgressBar.py
+++ ztools/ztools/ProgressBar.py
@@ -6,34 +6,38 @@
 # 变更履历：
 # 2018-09-05 | Zou Mingzhe   | Ver0.1  | 初始版本
 # ----------------------------------------------------------------------------------------------------
-# 代码说明：
-# ProgressBar类提供了进度条字符串。
-# ----------------------------------------------------------------------------------------------------
 # MAP：
 # 已测试 | Version(self, ...)               | 版本显示
 # 已测试 | PercentProgressBar(self, ...)    | 百分比进度条
 # 已测试 | NumberProgressBar(self, ...)     | 数字进度条
 # ----------------------------------------------------------------------------------------------------
-class ProgressBar:
+class ProgressBar: 
+    """
+    ProgressBar类提供了进度条字符串。
+    """
     def __init__(self):
         self.__version = "0.1"
         self.__progress = 0
         self.__progressbarlist = ['', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█', '█']
 # ----------------------------------------------------------------------------------------------------
-# 版本显示：
-# 输入参数：isShow = False
-# 返回参数：self.__version
-# 说明：调用该方法将返回类的版本号，若isShow == True则会在屏幕上打印版本号。
     def Version(self, isShow = False): 
+        """
+        版本显示：
+        输入参数：isShow = False
+        返回参数：self.__version
+        说明：调用该方法将返回类的版本号，若isShow == True则会在屏幕上打印版本号。
+        """
         if(isShow):
             print("[ztools]-[ProgressBar]-[vesion:%s]" % self.__version)
         return self.__version
 # ----------------------------------------------------------------------------------------------------
-# 百分比进度条：
-# 输入参数：progress
-# 返回参数：strprogressbar
-# 说明：调用该方法将返回百分比进度条，百分比进度条 = 百分比 + 进度条。progress必须介于0至1之间。
     def PercentProgressBar(self, progress):
+        """
+        百分比进度条：
+        输入参数：progress
+        返回参数：strprogressbar
+        说明：调用该方法将返回百分比进度条，百分比进度条 = 百分比 + 进度条。progress必须介于0至1之间。
+        """
         progress = progress * 100
         if(progress > 100):
             progress = 100
@@ -46,12 +50,14 @@ class ProgressBar:
         strprogressbar = strprogressbar + self.__progressbarlist[int(progress) % 10]
         return strprogressbar
 # ----------------------------------------------------------------------------------------------------
-# 数字进度条：
-# 输入参数：Done, Sum
-# 返回参数：strprogressbar
-# 说明：调用该方法将返回数字进度条，数字进度条 = Done/Sum + 进度条。
-# 其中，Done为已完成进度，Sum为总进度。
     def NumberProgressBar(self, Done, Sum):
+        """
+        数字进度条：
+        输入参数：Done, Sum
+        返回参数：strprogressbar
+        说明：调用该方法将返回数字进度条，数字进度条 = Done/Sum + 进度条。
+        其中，Done为已完成进度，Sum为总进度。
+        """
         if(Sum <= 0):
             Sum = 1
         if(Done < 0):
