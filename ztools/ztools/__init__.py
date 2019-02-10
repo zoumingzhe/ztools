5a91f62e618fc810bebd47fe763d3c2323ae0861
zoumingzhe <zoumingzhe@qq.com>
2019-02-11 06:14:04 +0800

    新增Plot
    新增Plot，并发布

--- ztools/ztools/__init__.py
+++ ztools/ztools/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2018 ZouMingzhe <zoumingzhe@qq.com>
 # This module is part of the ztools package, which is released under a MIT licence.
-from .Timeout import Timeout
+from .Plot import Plot
 from .ProgressBar import ProgressBar
+from .Timeout import Timeout
