29824cc4af99551c83433ee39e13fb5c62bb8a4c
zoumingzhe <zoumingzhe@qq.com>
2019-04-15 21:59:49 +0800

    Update Plot
    更新Plot模块，增加帮助信息。

--- ztools/ztools/Plot.py
+++ ztools/ztools/Plot.py
@@ -4,6 +4,7 @@
 # 类 Plot
 # ----------------------------------------------------------------------------------------------------
 # 变更履历：
+# 2019-04-15 | Zou Mingzhe   | Ver0.5  | 1.完善帮助信息
 # 2019-02-11 | Zou Mingzhe   | Ver0.4  | 1.增加 Save(self, path)
 #            |               |         | 2.增加 Read(self, path)
 #            |               |         | 3.增加 ShowImage(self, image)
@@ -39,7 +40,7 @@ class Plot:
     3、使用Scatter绘制散点图时，使用“点对象”作为参数，而不需要多个“向量”参数，并通过Point进一步简化了点的创建。
     """
     def __init__(self, title, size = None):
-        self.__version = "0.4"
+        self.__version = "0.5"
         self.__title   = title
         self.__figure  = plt.figure(num = self.__title, figsize = size)
         plt.title(self.__title)
@@ -58,81 +59,99 @@ class Plot:
             print("[ztools]-[Plot]-[vesion:%s]" % self.__version)
         return self.__version
 # ----------------------------------------------------------------------------------------------------
-# 保存绘图：
-# 输入参数：
-# 返回参数：
-# 说明：调用该方法将绘图保存为文件。
     def Save(self, path):
+        """
+        保存绘图：
+        输入参数：
+        返回参数：
+        说明：调用该方法将绘图保存为文件。
+        """
         fig = plt.figure(self.__title)
         fig.savefig(path)
 # ----------------------------------------------------------------------------------------------------
-# 读取文件：
-# 输入参数：
-# 返回参数：image 图像数据
-# 说明：调用该方法将读取图像文件。
     def Read(self, path):
+        """
+        读取文件：
+        输入参数：
+        返回参数：image 图像数据
+        说明：调用该方法将读取图像文件。
+        """
         image = img.imread(path)
         return image
 # ----------------------------------------------------------------------------------------------------
-# 显示绘图：
-# 输入参数：
-# 返回参数：
-# 说明：调用该方法将显示绘图。
     def Show(self):
+        """
+        显示绘图：
+        输入参数：
+        返回参数：
+        说明：调用该方法将显示绘图。
+        """
         plt.figure(self.__title)
         plt.show()
 # ----------------------------------------------------------------------------------------------------
-# 显示图像：
-# 输入参数：image 图像数据
-# 返回参数：
-# 说明：调用该方法将显示图像。
     def ShowImage(self, image):
+        """
+        显示图像：
+        输入参数：image 图像数据
+        返回参数：
+        说明：调用该方法将显示图像。
+        """
         plt.imshow(image) # 显示图片
         plt.axis('off')   # 不显示坐标轴
         plt.show()
 # ----------------------------------------------------------------------------------------------------
-# 添加坐标轴标签：
-# 输入参数：(xlabel = None, ylabel = None) x轴、y轴标签
-# 返回参数：
-# 说明：调用该方法并传入参数将给坐标轴添加标签，默认不添加，支持中文。
     def Label(self, xlabel = None, ylabel = None):
+        """
+        添加坐标轴标签：
+        输入参数：(xlabel = None, ylabel = None) x轴、y轴标签
+        返回参数：
+        说明：调用该方法并传入参数将给坐标轴添加标签，默认不添加，支持中文。
+        """
         plt.figure(self.__title)
         if(xlabel != None):
             plt.xlabel(xlabel)
         if(ylabel != None):
             plt.ylabel(ylabel)
 # ----------------------------------------------------------------------------------------------------
-# 绘制坐标轴：
-# 输入参数：
-# 返回参数：
-# 说明：调用该方法将绘制坐标轴。
     def Axis(self):
+        """
+        绘制坐标轴：
+        输入参数：
+        返回参数：
+        说明：调用该方法将绘制坐标轴。
+        """
         plt.figure(self.__title)
         # plt.axis["xzero"].set_visible(True)
         # plt.axis["xzero"].label.set_text("新建y=0坐标")
         # plt.axis["xzero"].label.set_color('green')
 # ----------------------------------------------------------------------------------------------------
-# 绘制网格线：
-# 输入参数：
-# 返回参数：
-# 说明：调用该方法将绘制网格线。
     def Grid(self):
+        """
+        绘制网格线：
+        输入参数：
+        返回参数：
+        说明：调用该方法将绘制网格线。
+        """
         plt.figure(self.__title)
         plt.grid(True)
 # ----------------------------------------------------------------------------------------------------
-# 获取一个点对象：
-# 输入参数：(x, y, size = None, color = None, marker = None) x、y、大小、颜色、形状
-# 返回参数：point
-# 说明：调用该方法将获取一个点对象，点的坐标（x、y）是必须的，其他可选。
-    def Point(self, x, y, size = None, color = None, marker = None): 
+    def Point(self, x, y, size = None, color = None, marker = None):
+        """
+        获取一个点对象：
+        输入参数：(x, y, size = None, color = None, marker = None) x、y、大小、颜色、形状
+        返回参数：point
+        说明：调用该方法将获取一个点对象，点的坐标（x、y）是必须的，其他可选。
+        """
         point = {'x':x, 'y':y, 'size':size, 'color':color, 'marker':marker}
         return point
 # ----------------------------------------------------------------------------------------------------
-# 绘制散点图：
-# 输入参数：point 可以是一个点对象，或是几个点对象组成的集合（列表）
-# 返回参数：
-# 说明：调用该方法将绘制散点图。
     def Scatter(self, point):
+        """
+        绘制散点图：
+        输入参数：point 可以是一个点对象，或是几个点对象组成的集合（列表）
+        返回参数：
+        说明：调用该方法将绘制散点图。
+        """
         plt.figure(self.__title)
         if(type(point) == dict):
             plt.scatter(x = point['x'], y = point['y'], s = point['size'],
