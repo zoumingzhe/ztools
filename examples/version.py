3ff46dbbeb84d6828e476a1692b7e8233341439a
zoumingzhe <zoumingzhe@qq.com>
2019-04-15 21:42:02 +0800

    Update example
    更新example例程version.py，打印Plot、File、Xls、MySQL版本号

--- examples/version.py
+++ examples/version.py
@@ -8,4 +8,20 @@ ztools_timeout.Version(isShow = True)
 ztools_progressbar = ztools.ProgressBar()
 ztools_progressbar.Version(isShow = True)
 
+# 显示 Plot 版本号
+ztools_plot = ztools.Plot("test")
+ztools_plot.Version(isShow = True)
+
+# 显示 File 版本号
+ztools_file = ztools.File()
+ztools_file.Version(isShow = True)
+
+# 显示 Xls 版本号
+ztools_xls = ztools.Xls()
+ztools_xls.Version(isShow = True)
+
+# 显示 MySQL 版本号
+ztools_mysql = ztools.MySQL()
+ztools_mysql.Version(isShow = True)
+
 input()
