c053828b01309cdc4926f8e644c06da475658c72
邹明哲 <zoumingzhe@qq.com>
2019-04-16 10:01:29 +0800

    Update setup.py
    更新版本

--- ztools/setup.py
+++ ztools/setup.py
@@ -18,7 +18,7 @@ except ImportError:
 setup(
     name="ztools",
     description="ToolBox for Python, Easy to Use.",
-    version="1.3.4",
+    version="1.3.5",
     author="Zou Mingzhe",
     author_email="zoumingzhe@qq.com",
     url="https://github.com/zoumingzhe/ztools",
