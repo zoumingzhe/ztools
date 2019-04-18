d949aa65ce0556d952fcea05f62eb800128fb2f1
Mingzhe Zou <zoumingzhe@qq.com>
2019-04-18 09:19:51 +0800

    Create ex_Timeout.py
    演示ztools.Timeout()的使用

---
+++ examples/ex_Timeout.py
@@ -0,0 +1,37 @@
+import ztools
+import time
+
+ztools_timeout = ztools.Timeout()
+ztools_timeout.Version(isShow = True)
+
+print("开始计时： %0.3f sec" % ztools_timeout.Time())
+time.sleep(0.1)
+print("延时 0.1 sec： %0.3f sec" % ztools_timeout.Time())
+print("TimeStart值： %0.3f sec" % ztools_timeout.GetTimeStart())
+
+print("重置计时： %0.3f sec" % ztools_timeout.Reset())
+time.sleep(1)
+print("延时 1 sec： %0.3f sec" % ztools_timeout.Time())
+print("TimeStart值： %0.3f sec" % ztools_timeout.GetTimeStart())
+
+print("重置计时： %0.3f sec" % ztools_timeout.Reset())
+print("超时判断（3 sec）：")
+for n in range(5):
+    time.sleep(1)
+    if ztools_timeout.Timeout(3):
+        print("计时 %d 时间： %0.3f sec，超时判断：True" % (n, ztools_timeout.Time()))
+    else:
+        print("计时 %d 时间： %0.3f sec，超时判断：False" % (n, ztools_timeout.Time()))
+print("TimeStart值：%0.3f sec" % ztools_timeout.GetTimeStart())
+
+print("重置计时： %0.3f sec" % ztools_timeout.Reset())
+print("超时判断（5 sec）：")
+for n in range(10):
+    time.sleep(1)
+    if ztools_timeout.Timeout(5):
+        print("计时 %d 时间： %0.3f sec，超时判断：True" % (n, ztools_timeout.Time()))
+    else:
+        print("计时 %d 时间： %0.3f sec，超时判断：False" % (n, ztools_timeout.Time()))
+print("TimeStart值： %0.3f sec" % ztools_timeout.GetTimeStart())
+
+input()
