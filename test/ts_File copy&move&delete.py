74d4c248de4368b16fa750417b217e56092930c7
Mingzhe Zou <zoumingzhe@qq.com>
2019-04-20 07:36:41 +0800

    Create ts_File copy&move&delete.py
    测试File模块的copy、move、delete功能

---
+++ test/ts_File copy&move&delete.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append(r'..\ztools\ztools')
+import File
+
+
+fil = File.File()
+fil.Version(isShow = True)
+fil.copy(r'.\ts_File\ts.txt', r'.\ts_File\folder1\test1.txt')
+input("按回车（Enter）继续")
+fil.copy(r'.\ts_File\ts.txt', r'.\ts_File\folder2\test2.txt')
+input("按回车（Enter）继续")
+fil.copy(r'.\ts_File\ts.txt', r'.\ts_File\test.txt')
+input("按回车（Enter）继续")
+fil.move(r'.\ts_File\test.txt', r'.\ts_File\testx.txt')
+input("按回车（Enter）继续")
+fil.delete(r'.\ts_File\testx.txt')
+input("按回车（Enter）继续")
