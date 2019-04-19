33347a74859e14058ca1b9a36af5d7ef439f41af
zoumingzhe <zoumingzhe@qq.com>
2019-04-20 07:36:56 +0800

    Create ts_File scan.py
    测试File模块的scan功能

---
+++ test/ts_File scan.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append(r'..\ztools\ztools')
+import File
+
+
+fil = File.File()
+fil.Version(isShow = True)
+input("按回车（Enter）扫描.\\ts_File文件夹下文件")
+res = fil.scan(r'.\ts_File')
+print(res)
+input("按回车（Enter）扫描.\\ts_File文件夹及其子文件夹下文件")
+res = fil.scan(r'.\ts_File', sub=True)
+print(res)
+input("按回车（Enter）扫描.\\ts_File文件夹及其子文件夹下sub开头文件")
+res = fil.scan(r'.\ts_File', sub=True, prefix="sub")
+print(res)
+input("按回车（Enter）扫描.\\ts_File文件夹及其子文件夹下.file文件")
+res = fil.scan(r'.\ts_File', sub=True, postfix=".file")
+print(res)
+input("按回车（Enter）继续")
