7b369f7a1ff41ae912db1adb476bb43fffa30d63
Mingzhe Zou <zoumingzhe@qq.com>
2019-04-30 14:10:49 +0800

    add test file
    增加测试文件

---
+++ test/ts_File ensure.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append(r'..\ztools\ztools')
+import File
+
+
+fil = File.File()
+fil.Version(isShow = True)
+path = '.\\ts_File\\ensure'
+
+print(path)
+print(fil.ensure(path, iscreate = False))
+input("按回车（Enter）继续")
+
+print(path)
+print(fil.ensure(path, iscreate = True))
+input("按回车（Enter）继续")
+
+print(path)
+print(fil.ensure(path, iscreate = False))
+input("按回车（Enter）继续")
