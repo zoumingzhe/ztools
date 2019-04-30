d2b64ba673d98c0e650d48288c548c1d8cd9c0f8
邹明哲 <zoumingzhe@qq.com>
2019-04-30 14:10:07 +0800

    update to version 1.4.5
    更新版本至1.4.5
    class File
    1.增加 ensure(self, path, iscreate = True)
    2.清楚无用的变量
    class Xls
    1.xls文件保存前确保路径存在

--- ztools/ztools/__init__.py
+++ ztools/ztools/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2018 ZouMingzhe <zoumingzhe@qq.com>
 # This module is part of the ztools package, which is released under a MIT licence.
-from  .MySQL         import  MySQL
-from  .Xls           import  Xls
 from  .File          import  File
-from  .Plot          import  Plot
-from  .ProgressBar   import  ProgressBar
 from  .Timeout       import  Timeout
+from  .ProgressBar   import  ProgressBar
+from  .Xls           import  Xls
+from  .MySQL         import  MySQL
+from  .Plot          import  Plot
