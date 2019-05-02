8146b98b7b347773672b4f7a46d88fbbfcea2bb2
zoumingzhe <zoumingzhe@qq.com>
2019-05-02 16:14:00 +0800

    update test file

--- test/ts_File ensure.py
+++ test/ts_File ensure.py
@@ -8,13 +8,13 @@ fil.Version(isShow = True)
 path = '.\\ts_File\\ensure'
 
 print(path)
-print(fil.ensure(path, iscreate = False))
+print(fil.ensure(path, isCreate = False))
 input("按回车（Enter）继续")
 
 print(path)
-print(fil.ensure(path, iscreate = True))
+print(fil.ensure(path, isCreate = True))
 input("按回车（Enter）继续")
 
 print(path)
-print(fil.ensure(path, iscreate = False))
+print(fil.ensure(path, isCreate = False))
 input("按回车（Enter）继续")
