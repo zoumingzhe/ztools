3d9db9a2dabd9859f11c61bbd037002c0abefa08
zoumingzhe <zoumingzhe@qq.com>
2019-05-03 21:28:40 +0800

    version 1.5.5
    更新ProgressBar进度条模块

--- ztools/ztools/ProgressBar.py
+++ ztools/ztools/ProgressBar.py
@@ -4,20 +4,27 @@
 # 类 ProgressBar
 # ----------------------------------------------------------------------------------------------------
 # 变更履历：
+# 2019-05-03 | Zou Mingzhe   | Ver0.2  | 1.增加 EOF(self, isClean = False)
+#            |               |         | 2.修改 PercentProgressBar(self, ...) 为 Percent(self, ...)
+#            |               |         | 3.修改 NumberProgressBar(self, ...)  为 Number(self, ...)
 # 2018-09-05 | Zou Mingzhe   | Ver0.1  | 初始版本
 # ----------------------------------------------------------------------------------------------------
 # MAP：
 # 已测试 | Version(self, ...)           | 版本显示
-# 已测试 | PercentProgressBar(self, ...)| 百分比进度条
-# 已测试 | NumberProgressBar(self, ...) | 数字进度条
+# 已测试 | Percent(self, ...)           | 百分比进度条
+# 已测试 | Number(self, ...)            | 数字进度条
+# 已测试 | EOF(self, ...)               | 结束符
+# ----------------------------------------------------------------------------------------------------
+import sys
 # ----------------------------------------------------------------------------------------------------
 class ProgressBar:
     """
     ProgressBar类提供了进度条字符串。
     """
     def __init__(self):
-        self.__version = "0.1"
+        self.__version = "0.2"
         self.__progress = 0
+        self.__maxlength = 0
         self.__progressbarlist = ['', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█', '█']
 # ----------------------------------------------------------------------------------------------------
     def Version(self, isShow = False):
@@ -31,10 +38,10 @@ class ProgressBar:
             print("[ztools]-[ProgressBar]-[vesion:%s]" % self.__version)
         return self.__version
 # ----------------------------------------------------------------------------------------------------
-    def PercentProgressBar(self, progress):
+    def Percent(self, progress, isShow = False):
         """
         百分比进度条：
-        输入参数：progress
+        输入参数：progress, isShow = False
         返回参数：strprogressbar
         说明：调用该方法将返回百分比进度条，百分比进度条 = 百分比 + 进度条。progress必须介于0至1之间。
         """
@@ -48,12 +55,16 @@ class ProgressBar:
         for n in range(int(progress / 10)):
             strprogressbar = strprogressbar + '█'
         strprogressbar = strprogressbar + self.__progressbarlist[int(progress) % 10]
+        self.__maxlength = max(len(strprogressbar), self.__maxlength)
+        if(isShow):
+            sys.stdout.write(strprogressbar + '\r')
+            sys.stdout.flush()
         return strprogressbar
 # ----------------------------------------------------------------------------------------------------
-    def NumberProgressBar(self, Done, Sum):
+    def Number(self, Done, Sum, isShow = False):
         """
         数字进度条：
-        输入参数：Done, Sum
+        输入参数：Done, Sum, isShow = False
         返回参数：strprogressbar
         说明：调用该方法将返回数字进度条，数字进度条 = Done/Sum + 进度条。
         其中，Done为已完成进度，Sum为总进度。
@@ -74,5 +85,25 @@ class ProgressBar:
         for n in range(int(progress / 10)):
             strprogressbar = strprogressbar + '█'
         strprogressbar = strprogressbar + self.__progressbarlist[int(progress) % 10]
+        self.__maxlength = max(len(strprogressbar), self.__maxlength)
+        if(isShow):
+            sys.stdout.write(strprogressbar + '\r')
+            sys.stdout.flush()
         return strprogressbar
+# ----------------------------------------------------------------------------------------------------
+    def EOF(self, isClean = False):
+        """
+        结束符：
+        输入参数：isClean = False
+        返回参数：
+        说明：调用该方法将在换行或清空屏幕进度条。
+        """
+        if(isClean):
+            for n in range(self.__maxlength):
+                sys.stdout.write(' ')
+                sys.stdout.flush()
+        else:
+            sys.stdout.write('\n')
+            sys.stdout.flush()
+        self.__maxlength = 0
 # ----------------------------------------------------------------------------------------------------
