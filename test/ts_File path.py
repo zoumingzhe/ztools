e368b9cb63b767c3c9f751b478c786a1b7f7dc19
邹明哲 <zoumingzhe@qq.com>
2019-05-23 11:55:44 +0800

    Create ts_File path.py
    增加File模块下path相关测试文件

---
+++ test/ts_File path.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append(r'..\ztools\ztools')
+import File
+
+
+fil = File.File()
+fil.Version(isShow = True)
+input("按回车（Enter）继续")
+res = fil.get_path(r'.\ts_File', ["test1", "test2"])
+print(res)
+print(fil.get_folder(res))
+print(fil.get_name(res))
+input("按回车（Enter）继续")
+res = fil.get_path(r'.\ts_File', "test")
+print(res)
+print(fil.get_folder(res))
+print(fil.get_name(res))
+input("按回车（Enter）继续")
