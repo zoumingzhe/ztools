a112416a452046cd0f3947dcf268cfcbd593b199
Mingzhe Zou <zoumingzhe@qq.com>
2020-09-23 16:19:40 +0800

    UPDATE DB
    add CRUD
    add SQLite

--- ztools/setup.py
+++ ztools/setup.py
@@ -18,7 +18,7 @@ except ImportError:
 setup(
     name="ztools",
     description="ToolBox for Python, Easy to Use.",
-    version="2.0.1",
+    version="2.1.10",
     author="Zou Mingzhe",
     author_email="zoumingzhe@qq.com",
     url="https://github.com/zoumingzhe/ztools",
--- ztools/ztools/db/SQLite.py
+++ ztools/ztools/db/SQLite.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
 # coding=utf-8
 # ----------------------------------------------------------------------------------------------------
-# 类 MySQL
+# 类 SQLite
 # ----------------------------------------------------------------------------------------------------
 # 变更履历：
-# 2019-04-14 | Zou Mingzhe   | Ver0.1  | 初始版本
+# 2020-09-20 | Zou Mingzhe   | Ver0.1  | 初始版本
 # ----------------------------------------------------------------------------------------------------
 # MAP：
 # 已测试 | Version(self, ...)           | 版本显示
 # 未测试 | link(self, ...)              | 
 # 未测试 | unlink(self)                 | 
-# 未测试 | version(self)                | 
 # ----------------------------------------------------------------------------------------------------
 from ztools import CRUD
-import pymysql
+import sqlite3
 # ----------------------------------------------------------------------------------------------------
-class MySQL(CRUD):
+class SQLite(CRUD):
     """
-    MySQL类提供了对MySQL数据库访问的封装，它是基于pymysql实现的。
+    SQLite类提供了对SQLite数据库访问的封装，它是基于sqlite3实现的。
     """
     def __init__(self):
         self.__version = "0.1"
@@ -34,17 +33,17 @@ class MySQL(CRUD):
         说明：调用该方法将返回类的版本号，若isShow == True则会在屏幕上打印版本号。
         """
         if(isShow):
-            print("[ztools]-[MySQL]-[vesion:%s]" % self.__version)
+            print("[ztools]-[SQLite]-[vesion:%s]" % self.__version)
         return self.__version
 # ----------------------------------------------------------------------------------------------------
-    def link(self, info):
+    def link(self, db):
         """
         打开数据库连接：
-        输入参数：info
+        输入参数：db
         返回参数：self.__db 数据库操作对象
         说明：调用该方法将返回数据库操作对象。
         """
-        self.db = pymysql.connect(host=info['host'], port=info['port'], user=info['user'], passwd=info['passwd'], db=info['db'], charset=info['charset'])
+        self.db = sqlite3.connect(db)
         return self.db
 # ----------------------------------------------------------------------------------------------------
     def unlink(self):
@@ -57,21 +56,4 @@ class MySQL(CRUD):
         self.db.close()
         self.db = None
         return self.db
-# ----------------------------------------------------------------------------------------------------
-    def version(self):
-        """
-        MySQL版本显示：
-        输入参数：
-        返回参数：
-        说明：调用该方法将返回所连接的MySQL版本号。
-        """
-        # 使用 cursor() 方法创建一个游标对象 cursor
-        cursor = self.db.cursor()
-        # 使用 execute() 方法执行 SQL 查询 
-        cursor.execute("SELECT VERSION()")
-        # 使用 fetchone() 方法获取单条数据.
-        data = cursor.fetchone()
-        # 关闭游标
-        cursor.close()
-        return("mysql version : " + data[0])
 # ----------------------------------------------------------------------------------------------------
