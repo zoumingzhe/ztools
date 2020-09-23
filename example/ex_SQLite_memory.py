c4cd82460a60aa472bd0e7ea93c5624aaf4fd0f3
邹明哲 <zoumingzhe@qq.com>
2020-09-23 16:19:40 +0800

    UPDATE DB
    add CRUD
    add SQLite

---
+++ example/ex_SQLite_memory.py
@@ -0,0 +1,19 @@
+from ztools import SQLite
+
+db = SQLite()
+db.link(":memory:")
+print(db.commit('''CREATE TABLE test
+      (ID INT PRIMARY KEY     NOT NULL,
+       NAME           TEXT    NOT NULL,
+       AGE            INT     NOT NULL,
+       ADDRESS        CHAR(50),
+       SALARY         REAL);'''))
+print(db.insert('test', {'ID':1,'NAME':'name1','AGE':28}))
+print(db.insert('test', {'ID':2,'NAME':'name2','AGE':28}))
+print(db.select('test'))
+print(db.update('test', {'NAME':'\'name0\'', 'AGE':29}, 'NAME = \'name1\''))
+print(db.update('test', {'AGE':27}, 'AGE = 28'))
+print(db.select('test'))
+print(db.delete('test', 'NAME = \'name2\''))
+print(db.select('test'))
+db.unlink()
