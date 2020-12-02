a78511e85d538fc7e8fc322a7372f31671a8a86b
Mingzhe Zou <zoumingzhe@qq.com>
2020-12-02 13:40:45 +0800

    update 2.1.12
    rename function

--- ztools/setup.py
+++ ztools/setup.py
@@ -18,7 +18,7 @@ except ImportError:
 setup(
     name="ztools",
     description="ToolBox for Python, Easy to Use.",
-    version="2.1.10",
+    version="2.1.12",
     author="Zou Mingzhe",
     author_email="zoumingzhe@qq.com",
     url="https://github.com/zoumingzhe/ztools",
