1e3348491765a5d7d8a21639c6ee8dbf3f872bf5
邹明哲 <zoumingzhe@qq.com>
2020-12-03 20:16:24 +0800

    Update ts_plot.py
    更新接口

--- test/ts_plot.py
+++ test/ts_plot.py
@@ -1,42 +1,46 @@
 import sys
 sys.path.append(r'..\ztools\ztools')
 from file.filebase import filebase
-from GUI.plot import plot
 from tool.timeout import timeout
+from GUI.plot import plot
 
 # 参考：
-# https://blog.csdn.net/weixin_40040404/article/details/81185564
+# https://matplotlib.org/api/pyplot_summary.html
 
 fb = filebase()
 fb.ensure('.\\ts_plot')
 
-plt = plot('测试Plot（scatter）')
+plt = plot()
 plt.Version(isShow = True)
-plt.label(xlabel='x轴', ylabel='y轴')
+plt.figure('测试Plot（scatter）')
+plt.title('测试Plot')
+plt.sub(121)
+plt.title('scatter')
+plt.label(xlabel='日期', ylabel='时间')
 points = []
 points.append(plt.point('星期一', '13:00', 10))
 points.append(plt.point('星期二', '14:30', 20))
 points.append(plt.point('星期三', '15:00', 30))
 points.append(plt.point('星期四', '16:00', 40))
 plt.scatter(points)
-plt.save('.\\ts_plot\\ts_plot_scatter.png')
-plt.show()
-
-plt = plot('测试Plot（xyscatter）')
-plt.Version(isShow = True)
+plt.sub(122)
+plt.title('xyscatter')
 plt.label(xlabel='x轴', ylabel='y轴')
 x = [1, 1, 2, 2]
 y = [1, 2, 1, 2]
 plt.xyscatter(x, y)
-plt.save('.\\ts_plot\\ts_plot_xyscatter.png')
-plt.show()
+plt.savefig('.\\ts_plot\\ts_plot_scatter.png')
+plt.close()
 
 timeout = timeout()
 timeout.Version(isShow = True)
+plt.figure('测试imread和imshow方法')
 print("开始： %0.3f sec" % timeout.Time())
-plt_image = plot('测试read和showimage方法')
-image = plt_image.read('.\\ts_Plot\\ts_plot_scatter.png')
+image = plt.imread('.\\ts_Plot\\ts_plot_scatter.png')
 print("用时： %0.3f sec" % timeout.Time())
-plt_image.showimage(image)
+plt.imshow(image)
+plt.close()
 
 input("按回车（Enter）继续")
+
+
