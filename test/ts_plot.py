c8f568ad05b53a7f6b2274771030ca115191ccbc
Mingzhe Zou <zoumingzhe@qq.com>
2020-12-03 17:06:50 +0800

    Update ts_plot.py

--- test/ts_plot.py
+++ test/ts_plot.py
@@ -1,44 +1,42 @@
 import sys
 sys.path.append(r'..\ztools\ztools')
+from file.filebase import filebase
 from GUI.plot import plot
 from tool.timeout import timeout
 
 # 参考：
 # https://blog.csdn.net/weixin_40040404/article/details/81185564
 
+fb = filebase()
+fb.ensure('.\\ts_plot')
 
-plt = plot('测试Plot')
+plt = plot('测试Plot（scatter）')
 plt.Version(isShow = True)
-plt.label(xlabel='x轴')
-# point = plt.point(0, 0, 50)
-# #print(point)
-# plt.Scatter(point)
-# points = [plt.point(1, 1, 10), plt.point(1, 2, 20), plt.point(2, 1, 30), plt.point(2, 2, 40)]
-# #print(points)
-# plt.Scatter(points)
+plt.label(xlabel='x轴', ylabel='y轴')
 points = []
 points.append(plt.point('星期一', '13:00', 10))
 points.append(plt.point('星期二', '14:30', 20))
 points.append(plt.point('星期三', '15:00', 30))
 points.append(plt.point('星期四', '16:00', 40))
 plt.scatter(points)
-#plt.scatter(1, 1)
-#plt.scatter(1, 2)
-#plt.scatter(2, 1)
-#plt.scatter(2, 2)
-#plt.grid()
-plt.save('.\\ts_plot\\ts_plot.png')
+plt.save('.\\ts_plot\\ts_plot_scatter.png')
 plt.show()
 
+plt = plot('测试Plot（xyscatter）')
+plt.Version(isShow = True)
+plt.label(xlabel='x轴', ylabel='y轴')
+x = [1, 1, 2, 2]
+y = [1, 2, 1, 2]
+plt.xyscatter(x, y)
+plt.save('.\\ts_plot\\ts_plot_xyscatter.png')
+plt.show()
 
-
-ztools_timeout = Timeout.Timeout()
-ztools_timeout.Version(isShow = True)
-plt_image = ztools.Plot(title = None)
-print("开始： %0.3f sec" % ztools_timeout.Time())
-#plt_image = Plot('测试Read和Image方法')
-image = plt_image.Read('.\\ts_Plot\\ts_Plot.png')
-plt_image.ShowImage(image)
-print("用时： %0.3f sec" % ztools_timeout.Time())
+timeout = timeout()
+timeout.Version(isShow = True)
+print("开始： %0.3f sec" % timeout.Time())
+plt_image = plot('测试read和showimage方法')
+image = plt_image.read('.\\ts_Plot\\ts_plot_scatter.png')
+print("用时： %0.3f sec" % timeout.Time())
+plt_image.showimage(image)
 
 input("按回车（Enter）继续")
