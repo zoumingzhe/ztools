3b545edca79a182031ef4eb775a4532018e4b767
Mingzhe Zou <zoumingzhe@qq.com>
2020-12-08 18:33:17 +0800

    add file hash
    增加md5、sha1两种文件hash

--- test/ts_hash.py
+++ test/ts_hash.py
@@ -5,12 +5,16 @@ from data.hash import hash
 
 hash = hash()
 
+print(hash.md5_file(".\\ts_hash\\ts_hash_md5.txt"))
 print(hash.md5("This a md5 test!".encode("utf-8")))
 data = "This a md5 test!".encode("utf-8")
 print(hash.md5(data))
+print(data)
 
-print(hash.sha1("This a md5 test!".encode("utf-8")))
-data = "This a md5 test!".encode("utf-8")
+print(hash.sha1_file(".\\ts_hash\\ts_hash_sha1.txt"))
+print(hash.sha1("This a sha1 test!".encode("utf-8")))
+data = "This a sha1 test!".encode("utf-8")
 print(hash.sha1(data))
+print(data)
 
 input("按回车（Enter）继续")
