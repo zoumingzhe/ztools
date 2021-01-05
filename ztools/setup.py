9a4d3a9e216ea94d4557056e1894cab4fd385450
Mingzhe Zou <zoumingzhe@qq.com>
2021-01-05 12:01:04 +0800

    update to 2.2.3

--- ztools/setup.py
+++ ztools/setup.py
@@ -18,7 +18,7 @@ except ImportError:
 setup(
     name="ztools",
     description="ToolBox for Python, Easy to Use.",
-    version="2.1.13",
+    version="2.2.3",
     author="Zou Mingzhe",
     author_email="zoumingzhe@qq.com",
     url="https://github.com/zoumingzhe/ztools",
