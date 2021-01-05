a11cfa3bfbb4c01eef9551465ef1e8595c2e6f03
Mingzhe Zou <zoumingzhe@qq.com>
2021-01-05 14:11:23 +0800

    Create ex_plot.py

---
+++ example/ex_plot.py
@@ -0,0 +1,14 @@
+from ztools import plot
+from ztools import fbasic
+from ztools import timeout
+
+# 参考：
+# https://matplotlib.org/api/pyplot_summary.html
+
+fb = fbasic()
+fb.ensure('.\\ex_plot')
+
+plt = plot()
+plt.Version(isShow = True)
+
+input("按回车（Enter）继续")
