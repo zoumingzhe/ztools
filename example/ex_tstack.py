9692a7d96cf79ffecbf50563d898a16047dcc882
Mingzhe Zou <zoumingzhe@qq.com>
2021-01-05 12:01:44 +0800

    update example

---
+++ example/ex_tstack.py
@@ -0,0 +1,21 @@
+from ztools import tstack
+
+s = tstack()
+#s.Version(isShow = True)
+
+print(s.get())
+s.put(1)
+print(s.get())
+s.put(2)
+s.put(3)
+s.put(4)
+print(s.get())
+s.put(5)
+print(s.get())
+print(s.get())
+print(s.get())
+print(s.get())
+print(s.get())
+
+
+input()
