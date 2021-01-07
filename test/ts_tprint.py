e1b8de928e11e8c00829cf637c062f9a8ab64ce6
邹明哲 <zoumingzhe@qq.com>
2021-01-07 17:09:52 +0800

    Update ts_tprint.py

--- test/ts_tprint.py
+++ test/ts_tprint.py
@@ -1,28 +1,42 @@
 import sys
 sys.path.insert(0, r'..\ztools')
 from ztools.tool.tprint import tprint
+from ztools.tool.tprint import AnsiStyle as style
+from ztools.tool.tprint import AnsiFore as fore
+from ztools.tool.tprint import AnsiBack as back
 
 tp = tprint()
-tp.color("123", style = 4, foreground = 31, background = 47)
+
+print("-----")
+tp.color("123", style.underline, fore.red, back.white)
 tp.flush()
-tp.color("123", style = 4, foreground = 31)
+tp.color("123", style.underline, fore.red)
 tp.flush()
-tp.color("123", style = 4, background = 47)
+tp.color("123", style.underline, back.white)
 tp.flush()
-tp.color("123", style = 4)
+tp.color("123", style.underline)
 tp.flush()
 tp.color("123")
 tp.flush()
 
 print("-----")
-
-tp.color("123", foreground = 31, background = 47)
+tp.color("123", fore.red, back.white)
 tp.flush()
-tp.color("123", foreground = 31)
+tp.color("123", fore.red)
 tp.flush()
-tp.color("123", background = 47)
+tp.color("123", back.white)
 tp.flush()
 tp.color("123")
+tp.flush()
+
+print("-----")
+tp.color(123, fore.red, back.white)
+tp.flush()
+tp.color(123, fore.red)
+tp.flush()
+tp.color(123, back.white)
+tp.flush()
+tp.color(123)
 tp.flush()
 
 input("按回车（Enter）继续")
