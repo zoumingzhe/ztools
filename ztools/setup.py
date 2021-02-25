674e20b50e9693f37010adbe02874763680b331b
邹明哲 <zoumingzhe@qq.com>
2021-02-25 11:51:54 +0800

    update tprint

--- ztools/setup.py
+++ ztools/setup.py
@@ -18,7 +18,7 @@ except ImportError:
 setup(
     name="ztools",
     description="ToolBox for Python, Easy to Use.",
-    version="2.3.0",
+    version="2.3.3",
     author="Zou Mingzhe",
     author_email="zoumingzhe@qq.com",
     url="https://github.com/zoumingzhe/ztools",
