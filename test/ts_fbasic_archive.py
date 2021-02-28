51d9ff7242057d47c3e7ed63775c4eec3ecf2be3
邹明哲 <zoumingzhe@outlook.com>
2021-02-28 15:07:17 +0800

    update test file

---
+++ test/ts_fbasic_archive.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('..\\ztools\\ztools')
+from data.fbasic import fbasic
+
+fbasic.archive('.\\ts_fbasic\\folder1')
+input("按回车（Enter）继续")
+fbasic.archive('.\\ts_fbasic\\folder1', '.\\ts_fbasic\\backup')
+input("按回车（Enter）继续")
+fbasic.archive('.\\ts_fbasic\\folder1', '.\\ts_fbasic', "backup1")
+input("按回车（Enter）继续")
+fbasic.archive('.\\ts_fbasic\\folder1', '.\\ts_fbasic', "backup1", "tar")
+input("按回车（Enter）继续")
+fbasic.archive_unpack('.\\ts_fbasic\\backup\\backup_folder1.zip')
+input("按回车（Enter）继续")
+fbasic.archive_unpack('.\\ts_fbasic\\backup1.tar', '.\\ts_fbasic\\backup_folder1')
+input("按回车（Enter）继续")
