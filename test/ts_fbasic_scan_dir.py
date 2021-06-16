5046b88f7c94890f33ee123bcaa75c87f443fd66
Mingzhe Zou <zoumingzhe@qq.com>
2021-06-16 16:58:06 +0800

    Update fbasic.py
    1.修改 scan(self, ...) 支持扫描目录，返回元组并且有

---
+++ test/ts_fbasic_scan_dir.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append(r'..\ztools\ztools')
+from data.fbasic import fbasic
+
+
+print("扫描.\\ts_fbasic目录下目录")
+for i in fbasic.scan(r'.\ts_fbasic', ret_dir = True, ret_file = False):
+    print(i)
+
+print("扫描.\\ts_fbasic目录及其子目录下目录")
+for i in fbasic.scan(r'.\ts_fbasic', sub=True, ret_dir = True, ret_file = False):
+    print(i)
+
+print("扫描.\\ts_fbasic2目录下目录")
+for i in fbasic.scan(r'.\ts_fbasic2', sub=True, ret_dir = True, ret_file = False):
+    print(i)
+
+input("按回车（Enter）继续")
