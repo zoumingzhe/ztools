91be72639dcb2da1430f42462b6b62cc9ad100fc
邹明哲 <zoumingzhe@outlook.com>
2021-07-16 02:01:47 +0800

    update hash

--- ztools/ztools/data/hash.py
+++ ztools/ztools/data/hash.py
@@ -11,25 +11,23 @@
 # 已测试 | sha1(self, ...)              | 计算sha1值
 # ----------------------------------------------------------------------------------------------------
 import hashlib
+import os
 # ----------------------------------------------------------------------------------------------------
 class hash():
     """
     hash类提供了哈希值计算接口。
     """
-    def __init__(self):
-        self.__version = "0.1"
 # ----------------------------------------------------------------------------------------------------
     @staticmethod
-    def __file_md5(filename):
+    def __file(h, fp):
         """
-        md5计算：
-        输入参数：filename 文件名
+        哈希计算：
+        输入参数：fp 文件路径
         返回参数：
-        说明：调用该方法将返回文件md5计算值。
+        说明：调用该方法将返回文件哈希计算值。
         """
         try:
-            with open(filename, 'rb') as f:
-                h = hashlib.md5()
+            with open(fp, 'rb') as f:
                 d = f.read(4096)
                 while d:
                     h.update(d)
@@ -49,48 +47,25 @@ class hash():
         """
         if not args and not kwargs:
             return hashlib.md5()
-        if 'filename' in kwargs:
-            filename = kwargs['filename']
-            x = type(filename)
-            if x is str:
-                return hash.__file_md5(filename)
+        if 'file' in kwargs:
+            f = kwargs['file']
+            x = type(f)
+            if x is str and os.path.isfile(f):
+                return hash.__file(hashlib.md5(), f)
             if x is list or x is tuple:
                 h = []
-                for name in filename:
-                    h.append(hash.__file_md5(name))
-                if x is tuple:
-                    h = tuple(h)
-                return h
-        encoding = None
-        if 'encoding' in kwargs:
-            encoding = kwargs['encoding']
-        for data in args:
-            x = type(data)
-            if x is bytes:
-                return hashlib.md5(data).hexdigest()
-            elif x is str:
-                return hashlib.md5(data.encode(encoding)).hexdigest()
-        return None
-# ----------------------------------------------------------------------------------------------------
-    @staticmethod
-    def __file_sha1(filename):
-        """
-        sha1计算：
-        输入参数：filename 文件名
-        返回参数：
-        说明：调用该方法将返回文件sha1计算值。
-        """
-        try:
-            with open(filename, 'rb') as f:
-                h = hashlib.sha1()
-                d = f.read(4096)
-                while d:
-                    h.update(d)
-                    d = f.read(4096)
-                f.close()
-                return h.hexdigest()
-        except:
+                for n in f:
+                    h.append(hash.__file(hashlib.md5(), n))
+                return h if x is list else tuple(h)
             return None
+        if not args:
+            return None
+        h = []
+        for data in args:
+            if type(data) is not bytes:
+                return None
+            h.append(hashlib.md5(data).hexdigest())
+        return h if len(h) > 1 else h[0]
 # ----------------------------------------------------------------------------------------------------
     @staticmethod
     def sha1(*args, **kwargs):
@@ -102,26 +77,23 @@ class hash():
         """
         if not args and not kwargs:
             return hashlib.sha1()
-        if 'filename' in kwargs:
-            filename = kwargs['filename']
-            x = type(filename)
-            if x is str:
-                return hash.__file_sha1(filename)
+        if 'file' in kwargs:
+            f = kwargs['file']
+            x = type(f)
+            if x is str and os.path.isfile(f):
+                return hash.__file(hashlib.sha1(), f)
             if x is list or x is tuple:
                 h = []
-                for name in filename:
-                    h.append(hash.__file_sha1(name))
-                if x is tuple:
-                    h = tuple(h)
-                return h
-        encoding = None
-        if 'encoding' in kwargs:
-            encoding = kwargs['encoding']
+                for n in f:
+                    h.append(hash.__file(hashlib.sha1(), n))
+                return h if x is list else tuple(h)
+            return None
+        if not args:
+            return None
+        h = []
         for data in args:
-            x = type(data)
-            if x is bytes:
-                return hashlib.sha1(data).hexdigest()
-            elif x is str:
-                return hashlib.sha1(data.encode(encoding)).hexdigest()
-        return None
+            if type(data) is not bytes:
+                return None
+            h.append(hashlib.sha1(data).hexdigest())
+        return h if len(h) > 1 else h[0]
 # ----------------------------------------------------------------------------------------------------
