922469393a4f573e78a99a29b72828fd26c0ec69
邹明哲 <zoumingzhe@outlook.com>
2021-08-20 21:33:22 +0800

    support .csv file support
    xls add .csv file support

--- ztools/ztools/__init__.py
+++ ztools/ztools/__init__.py
@@ -7,6 +7,7 @@ for common
 """
 from .common.file           import (fbasic)
 from .common.hash           import (hash)
+from .common.xls            import (csv)
 from .common.xls            import (xls)
 
 """
