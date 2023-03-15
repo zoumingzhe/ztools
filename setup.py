1b68f90c064796a7bec890a68e11b8fc717166f4
Mingzhe Zou <zoumingzhe@qq.com>
2023-03-16 01:52:47 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,55 +1,6 @@
-# setup.py for ztools
-#
-# Direct install (all systems):
-#   "python setup.py install"
-#
-# For Python 3.x use the corresponding Python executable,
-# e.g. "python3 setup.py ..."
-#
-# (C) 2018-2023 ZouMingzhe <zoumingzhe@qq.com>
-#
-# SPDX-License-Identifier:    MIT
+#!/usr/bin/python3
+# coding=utf-8
+import setuptools
 
-VERSION = "2.4.6"
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-setup(
-    name="ztools",
-    license="MIT",
-    version=VERSION,
-    author="Zou Mingzhe",
-    author_email="zoumingzhe@qq.com",
-    url="https://github.com/zoumingzhe/ztools",
-    description="ToolBox for Python, Easy to Use.",
-    packages=[
-        'ztools',
-        'ztools.common',
-        'ztools.db',
-        'ztools.ds',
-        'ztools.gui',
-        'ztools.utils',
-    ],
-    entry_points={
-        'console_scripts': [
-        ]
-    },
-    # long_description=open('.\README.rst', mode='r', encoding='UTF-8').read(),
-    install_requires=[
-        'psutil',
-        'colorama',
-        'matplotlib',
-        'pymysql',
-        'xlrd<=1.2.0',
-        'xlwt',
-    ],
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Natural Language :: Chinese (Simplified)',
-    ],
-    platforms='any',
-)
+if __name__ == "__main__":
+    setuptools.setup()
--- ztools/__init__.py
+++ ztools/__init__.py
@@ -1,37 +1,31 @@
 # Copyright (c) 2018-2023 ZouMingzhe <zoumingzhe@qq.com>
 
-
+__version__ = "2.4.7"
 """
 for common
 """
-from .common.file           import (fbasic)
-from .common.hash           import (hash)
-from .common.sheet          import (csv, xls)
-
+from .common.file import (fbasic)
+from .common.hash import (hash)
+from .common.sheet import (csv, xls)
 """
 for database
 """
-from .db.CRUD               import (CRUD)
-from .db.MySQL              import (MySQL)
-from .db.SQLite             import (SQLite)
-
+from .db.CRUD import (CRUD)
+from .db.MySQL import (MySQL)
+from .db.SQLite import (SQLite)
 """
 for data structure
 """
-from .ds.tqueue             import (tqueue)
-from .ds.tstack             import (tstack)
-
-
+from .ds.tqueue import (tqueue)
+from .ds.tstack import (tstack)
 """
 for gui
 """
-from .gui.plot              import (plot)
-
-
+from .gui.plot import (plot)
 """
 for utils
 """
-from .utils.progressbar     import (progressbar)
-from .utils.timeout         import (timeout)
-from .utils.zemail          import (mail)
-from .utils.zprint          import (AnsiStyle, AnsiFore, AnsiBack, zprint)
+from .utils.progressbar import (progressbar)
+from .utils.timeout import (timeout)
+from .utils.zemail import (mail)
+from .utils.zprint import (AnsiStyle, AnsiFore, AnsiBack, zprint)
